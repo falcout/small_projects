41335afc9bb6ad46a0cec5a2e54ac8441244278f
falcout <filipi_mathaus@outlook.com>
2026-06-04 15:15:16 -0300

    Add game over screen

--- space shooter/main.py
+++ space shooter/main.py
@@ -113,6 +113,7 @@ def collisions():
         damage_sound.play()
         player.life -= 1
     if player.life == 0:
+        player.kill()
         running = False
      
     for laser in laser_sprites:
@@ -122,12 +123,29 @@ def collisions():
             Explosion(explosion_frames, laser.rect.midtop, all_sprites)
             explosion_sound.play()
 
-def display_score():
-    current_time = pygame.time.get_ticks() // 100
+def display_score(time):
+    current_time = time
     text_surf = font.render(str(current_time), True, '#f0f0f0')
     text_rect = text_surf.get_frect(midbottom = (WINDOW_WIDTH / 2, WINDOW_HEIGHT - 50))
     screen.blit(text_surf, text_rect)
     pygame.draw.rect(screen, '#f0f0f0', text_rect.move(0, -8).inflate(20,10), 5, 10)
+
+def display_game_over():
+    over_surf = font.render('GAME OVER!', True, '#f0f0f0')
+    over_rect = over_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 - 50))
+    pygame.draw.rect(screen, 'black', over_rect.inflate(80,160).move(0, 55))
+    pygame.draw.rect(screen, '#f0f0f0', over_rect.inflate(80,160).move(0, 55), 5)
+    screen.blit(over_surf, over_rect)
+
+    restart_surf = buttom_font.render('Restart', True, "#38d035")
+    restart_rect = restart_surf.get_frect(midtop = over_rect.move(0,20).midbottom)
+    pygame.draw.rect(screen, '#f0f0f0', restart_rect.inflate(120,0).move(0,-5))
+    screen.blit(restart_surf, restart_rect)
+
+    quit_surf = buttom_font.render('Quit', True, "#8b0a20")
+    quit_rect = quit_surf.get_frect(midtop = restart_rect.move(0,18).midbottom)
+    pygame.draw.rect(screen, '#f0f0f0', restart_rect.inflate(120,0).move(0,50))
+    screen.blit(quit_surf, quit_rect)
 
 # general setup
 pygame.init()
@@ -143,6 +161,7 @@ meteor_surf = pygame.image.load(join('images', 'meteor.png')).convert_alpha()
 laser_surf = pygame.image.load(join('images', 'laser.png')).convert_alpha()
 explosion_frames = [pygame.image.load(join('images', 'explosion', f'{i}.png')).convert_alpha() for i in range(21)]
 font = pygame.font.Font(join('images', 'Oxanium-Bold.ttf'), 40)
+buttom_font = pygame.font.Font(join('images', 'Oxanium-Bold.ttf'), 30)
 
 laser_sound = pygame.mixer.Sound(join('audio', 'laser.wav'))
 laser_sound.set_volume(0.5)
@@ -156,6 +175,7 @@ game_music.play(loops=-1)
 all_sprites = pygame.sprite.LayeredUpdates()
 meteor_sprites = pygame.sprite.Group()
 laser_sprites = pygame.sprite.Group()
+final_boom = pygame.sprite.Group()
 for _ in range(30):
     Star(all_sprites, (randint(0, WINDOW_WIDTH), randint(-WINDOW_HEIGHT, WINDOW_HEIGHT)), star_surf)
 player = Player(all_sprites)
@@ -187,12 +207,38 @@ while running:
 
     # draw the game
     screen.fill('#3a2e3f')
-    display_score()
+    display_score(pygame.time.get_ticks() // 100)
     all_sprites.draw(screen)
 
 
     pygame.display.flip()
 
-# pygame.mixer.stop()
+pygame.mixer.stop()
+final_explosion = 0
+score = pygame.time.get_ticks() // 100
+frozen_frame = screen.copy()
+
+while not running:
+    dt = clock.tick() / 1000
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = True
+    
+    # update
+    final_explosion += 40 * dt
+    if final_explosion < 100:
+        Explosion(explosion_frames, (randint(0, WINDOW_WIDTH),randint(0,WINDOW_HEIGHT)), final_boom)
+        explosion_sound.play()
+
+    final_boom.update(dt)
+
+    # draw game_over screen
+    # screen.fill('#3a2e3f')
+    screen.blit(pygame.transform.grayscale(frozen_frame), (0,0))
+    final_boom.draw(screen)
+    display_score(score)
+    display_game_over()
+
+    pygame.display.flip()
 
 pygame.quit()
